69403ad1b62f9936fb5fe459722ab2670b8ad870
arian <arianparrot@gmail.com>
2023-11-04 17:32:32 +0330

    files added

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿let inputname = document.querySelector('#container__child--input-nameid')
+let inputpassword = document.querySelector('#container__child--input-passwordid')
+let modal =document.querySelector('.modal')
+function check(){
+  let inputnamevalue = inputname.value;
+  let inputpasswordvalue =inputpassword.value;
+if(inputnamevalue.length<12 || inputpasswordvalue.length<8){
+    modal.style.display= 'flex'
+}
+else{
+    modal.style.display= "flex"
+    modal.style.backgroundColor="#00C853"
+    modal.style.alignItems="center"
+    modal.style.justifyContent="center"
+    modal.innerHTML="welcome"
+    
+}
+modal.style.animationName="fade"
+modal.style.animationDuration="8000ms"
+
+setTimeout(function(){
+  modal.style.display= 'none'
+}, 7900);
+}
